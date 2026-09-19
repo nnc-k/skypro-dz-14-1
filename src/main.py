e198d6cb219d80bc8ab06c966e34032c9b28078d
nancy k <katkova.a1997@gmail.com>
2026-09-19 17:37:51 +0400

    пройдена проверка линтерами

--- src/main.py
+++ src/main.py
@@ -88,7 +88,7 @@ if __name__ == "__main__":
     print(category1.category_count)
     print(category1.product_count)
 
-    product4 = Product("55\" QLED 4K", "Фоновая подсветка", 123000.0, 7)
+    product4 = Product('55" QLED 4K', "Фоновая подсветка", 123000.0, 7)
     category2 = Category(
         "Телевизоры",
         "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником",
